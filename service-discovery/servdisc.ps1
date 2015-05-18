34f3081505471435866dff8d7ad734511446c42c
Raymond Kuiper <qix@the-wired.net>
2015-05-18 17:17:44 +0200

    Experimental change in regex

--- service-discovery/servdisc.ps1
+++ service-discovery/servdisc.ps1
@@ -15,9 +15,10 @@ $temp = 1
 # For each object in the list of services, print the output of the JSON message with the object properties that we are interessted in
 foreach ($objItem in $colItems) {
  $exe_dir = $objItem.PathName
+ $exe_dir = $exe_dir -replace '\s.*$',''
  $exe_dir = $exe_dir -replace '"?(.+\\).+exe.*$','$1'
  $exe_dir = $exe_dir -replace '\\','/'
- $exe_dir = $exe_dir -replace '"','\"'
+ $exe_dir = $exe_dir -replace '"',''
  
  $desc_val = $objItem.Description
  $desc_val = $desc_val -replace '\"','@'
